ac19e3c8e16c0a7291e0fa50a94393928c15210c
Jack Rickards <jackrickards@monzo.com>
2023-01-14 15:18:22 +0000

    fix slowness using nvimtree in a big monorepo

--- nvim/.config/nvim/lua/nvimtree-plug.lua
+++ nvim/.config/nvim/lua/nvimtree-plug.lua
@@ -15,4 +15,22 @@ require 'nvim-tree'.setup {
     renderer = {
         highlight_opened_files = "all",
     },
+    filesystem_watchers = {
+        enable = true,
+        debounce_delay = 50,
+        -- filesystem_watchers in wearedev make changing services reeeeaally slow
+        -- but this setting will mean changes in the filesystem are not going
+        -- to be reflected until you open a new session
+        ignore_dirs = { '/Users/jackrickards/src/github.com/monzo/wearedev' },
+    },
+    -- log = {
+    --     enable = true,
+    --     truncate = true,
+    --     types = {
+    --         diagnostics = true,
+    --         git = true,
+    --         profile = true,
+    --         watcher = true,
+    --     },
+    -- },
 }
