187da235e1e46bd60b676267625ec4e9ad3f811b
Jack Rickards <jackrickards@monzo.com>
2023-01-31 10:37:28 +0000

    telescope refactor

--- nvim/.config/nvim/lua/telescope-plug.lua
+++ nvim/.config/nvim/lua/telescope-plug.lua
@@ -4,14 +4,21 @@ vim.keymap.set('n', '<Leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<Leader>fh', '<cmd>Telescope help_tags<cr>')
 vim.keymap.set('n', '<Leader>fo', '<cmd>Telescope oldfiles<cr>')
 
-vim.my_fd = function(opts)
-    opts = opts or {}
-    opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-    require 'telescope.builtin'.find_files(opts)
-end
+--vim.my_fd = function(opts)
+--    opts = opts or {}
+--    opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+--    require 'telescope.builtin'.find_files(opts)
+--end
 
 -- allows telescope to search from the git repo not just the current dir
-vim.g.rooter_patterns = { '.git', '.svn', 'package.json', '!node_modules' }
-
-
-
+-- vim.g.rooter_patterns = { '.git', '.svn', 'package.json', '!node_modules' }
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            n = {
+                ["<C-y>"] = require('telescope.actions').add_selected_to_qflist +
+                    require('telescope.actions').open_qflist
+            },
+        },
+    },
+})
