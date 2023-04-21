7e79a34b782d3abb840426964279080f91335744
Jack Rickards <delabere@jacks-mbp.lan>
2023-04-21 01:49:26 +0100

    custom dap and lsp

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
 
     -- dap-core
-    { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
 
     -- import/override with your plugins
     { import = "plugins" },
