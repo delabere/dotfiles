6e66d764b5cb956968d4db2b9c75e0a3535d5ff0
Jack Rickards <delabere@jacks-mbp.lan>
2023-04-25 00:07:31 +0100

    ensure a few things are installed

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "debugpy",
+        "go-debug-adapter",
+        "gopls",
+        "pyright",
+        "rnix-lsp",
+        "shfmt",
+        "stylua",
+        -- "flake8",
+      },
+    },
+  },
+}
