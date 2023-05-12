7cf06a5b99c82d5f8115074b14cc56acb002b654
Jack Rickards <delabere@jacks-mbp.lan>
2023-05-12 14:41:03 +0100

    use autocommands for autosave rather than autosave plugs

--- nvim/.config/nvim/lua/plugins/small_plugins.lua
+++ nvim/.config/nvim/lua/plugins/small_plugins.lua
@@ -4,7 +4,7 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
+    dependencies = { "mason.nvim", "ThePrimeagen/refactoring.nvim" },
     opts = function()
       local nls = require("null-ls")
       return {
@@ -14,8 +14,9 @@ return {
           nls.builtins.diagnostics.fish,
           nls.builtins.formatting.stylua,
           nls.builtins.formatting.shfmt,
-          nls.builtins.formatting.black,
           nls.builtins.formatting.isort,
+          nls.builtins.formatting.black,
+          -- nls.builtins.code_actions.refactoring,
           -- nls.builtins.diagnostics.flake8,
         },
       }
@@ -23,7 +24,7 @@ return {
   },
 
   { "delabere/protodef" },
-
+  { "folke/zen-mode.nvim" },
   {
     "fatih/vim-go",
     make = ":GoInstallBinaries",
@@ -98,6 +99,41 @@ return {
   { "tpope/vim-surround" },
 
   -- { "907th/vim-auto-save" },
+  -- {
+  --   "Pocco81/auto-save.nvim",
+  --   opts = {
+  --     enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+  --     execution_message = {
+  --       message = function() -- message to print on save
+  --         return ""
+  --       end,
+  --       dim = 0.18, -- dim the color of `message`
+  --       cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+  --     },
+  --     events = { "InsertLeave" }, -- vim events that trigger auto-save. See :h events
+  --     -- function that determines whether to save the current buffer or not
+  --     -- return true: if buffer is ok to be saved
+  --     -- return false: if it's not ok to be saved
+  --     condition = function(buf)
+  --       local fn = vim.fn
+  --       local utils = require("auto-save.utils.data")
+  --
+  --       if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+  --         return true -- met condition(s), can save
+  --       end
+  --       return false -- can't save
+  --     end,
+  --     write_all_buffers = false, -- write all buffers when the current one meets `condition`
+  --     debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+  --     callbacks = { -- functions to be executed at different intervals
+  --       enabling = nil, -- ran when enabling auto-save
+  --       disabling = nil, -- ran when disabling auto-save
+  --       before_asserting_save = nil, -- ran before checking `condition`
+  --       before_saving = nil, -- ran before doing the actual save
+  --       after_saving = nil, -- ran after doing the actual save
+  --     },
+  --   },
+  -- },
 
   -- auto save all open buffers on any file change
   -- {
@@ -156,7 +192,8 @@ return {
   { "folke/zen-mode.nvim" },
   { "iamcco/markdown-preview.nvim" },
 
-  { -- the default blue for todo's is a little garish
+  {
+    -- the default blue for todo's is a little garish
     "folke/todo-comments.nvim",
     opts = {
       keywords = {
