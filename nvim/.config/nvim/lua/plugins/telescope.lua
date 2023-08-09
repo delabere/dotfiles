882384f7e0f333724deabc7f5194cb54fef6706a
Jack Rickards <jackrickards@monzo.com>
2023-08-09 11:48:02 +0100

    use the no_cd jump to service option

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
       require("telescope").load_extension("changed_files")
     end,
     keys = {
-      { "<leader>fs", ":Monzo jump_to_component<CR>", desc = "Monzo jump to component" },
+      { "<leader>fs", ":Monzo jump_to_component_no_cd<CR>", desc = "Monzo jump to component" },
       { "<leader>fd", ":Monzo jump_to_downstream<CR>", desc = "Monzo jump to downstream" },
     },
   },
