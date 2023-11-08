28c077a4e5751f174b12a7d250cd8602b0f36e63
Jack Rickards <delabere@jacks-mbp.lan>
2023-11-08 00:55:35 +0000

    formatting and lockfile

--- flake.nix
+++ flake.nix
@@ -10,32 +10,32 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... } @ inputs:
-    let
-      mkHomeManagerConfig = module: system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [
-            module
-          ];
-
-          extraSpecialArgs = {
-            inherit inputs system;
-          };
-        };
-    in
-    {
-      homeConfigurations = {
-        delabere-aarch64-darwin = mkHomeManagerConfig ./delabere.nix "aarch64-darwin";
-        delabere-x86_64-linux = mkHomeManagerConfig ./delabere.nix "x86_64-linux";
-        work-aarch64-darwin = mkHomeManagerConfig ./work.nix "aarch64-darwin";
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    mkHomeManagerConfig = module: system: let
+      pkgs = import nixpkgs {
+        inherit system;
       };
-    };
-}
+    in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
+        modules = [
+          module
+        ];
+
+        extraSpecialArgs = {
+          inherit inputs system;
+        };
+      };
+  in {
+    homeConfigurations = {
+      delabere-aarch64-darwin = mkHomeManagerConfig ./delabere.nix "aarch64-darwin";
+      delabere-x86_64-linux = mkHomeManagerConfig ./delabere.nix "x86_64-linux";
+      work-aarch64-darwin = mkHomeManagerConfig ./work.nix "aarch64-darwin";
+    };
+  };
+}
