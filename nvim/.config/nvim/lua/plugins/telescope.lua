74b374bde803772081bd5295977531ab08245e2d
Jack Rickards <delabere@Jacks-MacBook-Pro.local>
2023-11-24 23:39:47 +0000

    formatting

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,16 @@
 return {
 
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      { dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo", "axkirillov/telescope-changed-files" },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            { dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo", "axkirillov/telescope-changed-files" },
+        },
+        config = function()
+            require("telescope").load_extension("changed_files")
+        end,
+        keys = {
+            { "<leader>fs", ":Monzo jump_to_component_no_cd<CR>", desc = "Monzo jump to component" },
+            { "<leader>fd", ":Monzo jump_to_downstream<CR>",      desc = "Monzo jump to downstream" },
+        },
     },
-    config = function()
-      require("telescope").load_extension("changed_files")
-    end,
-    keys = {
-      { "<leader>fs", ":Monzo jump_to_component_no_cd<CR>", desc = "Monzo jump to component" },
-      { "<leader>fd", ":Monzo jump_to_downstream<CR>", desc = "Monzo jump to downstream" },
-    },
-  },
 }
