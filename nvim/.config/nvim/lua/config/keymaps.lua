d762580f8025ea142d75e18e3a9c34de42114fd7
Jack Rickards <jackrickards@monzo.com>
2023-12-17 17:55:26 +0000

    move some options from keymaps -> option

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -53,35 +53,6 @@ vim.keymap.set("n", "<Leader>k", ":cnext<CR>")
 -- stops p in visual mode yanking the replaced text
 vim.keymap.set("x", "p", "pgvy")
 
--- -- options
-vim.opt.tabstop = 4 -- show existing tab with 4 spaces width
-vim.opt.shiftwidth = 4 -- when indenting with '>', use 4 spaces width
-vim.opt.expandtab = true -- On pressing tab, insert 4 spaces
--- vim.opt.encoding = 'UTF-8'
--- vim.opt.hidden = true -- preserve background buffers
--- vim.opt.hlsearch = true -- highlights search terms
--- vim.opt.number = true -- linenumbers
--- vim.opt.relativenumber = true -- sets relative numbers
--- vim.opt.laststatus = 2
--- vim.opt.vb = true
--- vim.opt.ruler = true
--- vim.opt.spelllang = 'en_us'
--- vim.opt.autoindent = true
--- vim.opt.colorcolumn = '120'
--- vim.opt.mouse = 'a'
--- vim.opt.clipboard = 'unnamed'
--- --vim.opt.noscrollbind = true
--- vim.opt.wildmenu = true
--- vim.opt.autochdir = true
--- vim.api.nvim_command("set noswapfile")
--- vim.opt.scrolloff = 16 -- starts page scrolling when n lines from top/bottom
-vim.api.nvim_command("set nofoldenable") -- no folds please
--- vim.opt.signcolumn = 'yes' --creates a dedicated column for git status so line numbers still show up
--- vim.opt.ignorecase = true --ignore case in searching
--- vim.opt.splitbelow = true --force new splits to go to the right of current window--
--- vim.opt.splitright = true --force new splits to go to below the of current window
--- vim.opt.termguicolors = true
-
 -- this bit of code allows you to highlight a visual range
 -- and run a macro on each line
 vim.cmd("xnoremap @ :<C-u>call ExecuteMacroOverVisualRange()<CR>")
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -13,3 +13,32 @@ vim.opt.undolevels = vim.opt.undolevels
 
 -- sets the base_branch for use by the changed_files telescope picker
 vim.g.telescope_changed_files_base_branch = "master"
+
+-- -- options
+vim.opt.tabstop = 4 -- show existing tab with 4 spaces width
+vim.opt.shiftwidth = 4 -- when indenting with '>', use 4 spaces width
+vim.opt.expandtab = true -- On pressing tab, insert 4 spaces
+-- vim.opt.encoding = 'UTF-8'
+-- vim.opt.hidden = true -- preserve background buffers
+-- vim.opt.hlsearch = true -- highlights search terms
+-- vim.opt.number = true -- linenumbers
+-- vim.opt.relativenumber = true -- sets relative numbers
+-- vim.opt.laststatus = 2
+-- vim.opt.vb = true
+-- vim.opt.ruler = true
+-- vim.opt.spelllang = 'en_us'
+-- vim.opt.autoindent = true
+-- vim.opt.colorcolumn = '120'
+-- vim.opt.mouse = 'a'
+-- vim.opt.clipboard = 'unnamed'
+-- --vim.opt.noscrollbind = true
+-- vim.opt.wildmenu = true
+-- vim.opt.autochdir = true
+-- vim.api.nvim_command("set noswapfile")
+-- vim.opt.scrolloff = 16 -- starts page scrolling when n lines from top/bottom
+vim.api.nvim_command("set nofoldenable") -- no folds please
+-- vim.opt.signcolumn = 'yes' --creates a dedicated column for git status so line numbers still show up
+-- vim.opt.ignorecase = true --ignore case in searching
+-- vim.opt.splitbelow = true --force new splits to go to the right of current window--
+-- vim.opt.splitright = true --force new splits to go to below the of current window
+-- vim.opt.termguicolors = true
