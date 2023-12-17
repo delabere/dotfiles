9c593b0af7d9a6f38fe26aad04774a03e6d20b44
Jack Rickards <delabere@jacks-mbp.lan>
2023-12-17 21:43:36 +0000

    add neotest and neotest go

--- nvim/.config/nvim/lua/plugins/small_plugins.lua
+++ nvim/.config/nvim/lua/plugins/small_plugins.lua
@@ -239,4 +239,11 @@ return {
   {
     "axkirillov/telescope-changed-files",
   },
+  {
+    "nvim-neotest/neotest-go",
+  },
+  {
+    "nvim-neotest/neotest",
+    opts = { adapters = { "neotest-go" } },
+  },
 }
