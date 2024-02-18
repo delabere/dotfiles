b55434a3d5f7fe875c855ce3fe73bd7c9f5e809b
delabere <jack.rickards@hotmail.co.uk>
2024-02-18 21:06:05 +0000

    clean up all .nix file comments

--- lakeview.nix
+++ lakeview.nix
@@ -20,28 +20,11 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-    #(pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-    # pkgs.tmux
     pkgs.alejandra
     pkgs.delve
     pkgs.go
@@ -93,44 +76,6 @@
           home-manager -f $HOME/.dotfiles/configuration.nix switch "$@"
         }
 
-        function todo() {
-            [ ! -d "$HOME/notes" ] && mkdir "$HOME/notes"
-            [ ! -f "$HOME/notes/todo.md" ] && touch "$HOME/notes/todo.md"
-            nvim "$HOME/notes/todo.md"
-        }
-
-        function note() {
-            [ ! -d "$HOME/notes" ] && mkdir -p "$HOME/notes"
-            [ ! -f "$HOME/notes/$1.md" ] && touch "$HOME/notes/$1.md"
-            nvim "$HOME/notes/$1.md"
-        }
-
-        # for maintaining and reading a simple braglist
-        function brag() {
-            [ ! -f "$HOME/brag.md" ] && touch "$HOME/brag.md"
-            if [[ -z $1 ]]
-            then
-              cat $HOME/brag.md
-            else
-              echo "$(date +%d/%m/%Y) | $1" >> $HOME/brag.md
-            fi
-        }
-
-        # for maintaining and reading a simple learnlist
-        function learnit() {
-            [ ! -f "$HOME/learnit.txt" ] && touch "$HOME/learnit.txt"
-            if [[ -z $1 ]]
-            then
-              cat $HOME/learnit.txt
-            else
-              echo "$(date +%d/%m/%Y) | $1" >> $HOME/learnit.txt
-            fi
-        }
-
-        function gitprune() {
-          git fetch --all -p; git branch -vv | grep ": gone]" | awk '{ print $1 }' | xargs -n 1 git branch -D
-        }
-
         alias lg='lazygit'
         alias gcm='git checkout master && git pull'
 
@@ -149,25 +94,6 @@
         bindkey "^[f" forward-word
         bindkey "^[b" backward-word
 
-      '';
-
-      envExtra = ''
-        # work configuration
-        [ -f $HOME/src/github.com/monzo/starter-pack/zshenv ] && source $HOME/src/github.com/monzo/starter-pack/zshenv
-
-        # work related stuff
-        s101 () {
-          shipper deploy --s101 $1
-        }
-
-        shipthis () {
-          branch=$(eval "git rev-parse --symbolic-full-name --abbrev-ref HEAD")
-          shipper deploy --s101 $branch
-        }
-
-        prod () {
-          shipper deploy --prod $1
-        }
       '';
     };
 
@@ -237,31 +163,6 @@
     };
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/delabere/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nvim";
   };
