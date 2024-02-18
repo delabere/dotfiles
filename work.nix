eef219582c617cb2afde38e1a93e6570a5bc362b
delabere <jack.rickards@hotmail.co.uk>
2024-02-18 21:00:55 +0000

    clean up functions that are now in work-apps.nix

--- work.nix
+++ work.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     ./base.nix
+    ./base-apps.nix
     ./work-apps.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
@@ -57,18 +58,6 @@
           home-manager -f $HOME/.dotfiles/configuration.nix switch "$@"
         }
 
-        function todo() {
-            [ ! -d "$HOME/notes" ] && mkdir "$HOME/notes"
-            [ ! -f "$HOME/notes/todo.md" ] && touch "$HOME/notes/todo.md"
-            nvim "$HOME/notes/todo.md"
-        }
-
-        function note() {
-            [ ! -d "$HOME/notes" ] && mkdir -p "$HOME/notes"
-            [ ! -f "$HOME/notes/$1.md" ] && touch "$HOME/notes/$1.md"
-            nvim "$HOME/notes/$1.md"
-        }
-
         # for maintaining and reading a simple braglist
         function brag() {
             [ ! -f "$HOME/brag.md" ] && touch "$HOME/brag.md"
@@ -78,21 +67,6 @@
             else
               echo "$(date +%d/%m/%Y) | $1" >> $HOME/brag.md
             fi
-        }
-
-        # for maintaining and reading a simple learnlist
-        function learnit() {
-            [ ! -f "$HOME/learnit.txt" ] && touch "$HOME/learnit.txt"
-            if [[ -z $1 ]]
-            then
-              cat $HOME/learnit.txt
-            else
-              echo "$(date +%d/%m/%Y) | $1" >> $HOME/learnit.txt
-            fi
-        }
-
-        function gitprune() {
-          git fetch --all -p; git branch -vv | grep ": gone]" | awk '{ print $1 }' | xargs -n 1 git branch -D
         }
 
         alias lg='lazygit'
@@ -129,89 +103,6 @@
         [ -f $HOME/.dotfiles/env.sh ] && source $HOME/.dotfiles/env.sh
 
         JAVA_HOME=$(/usr/libexec/java_home -v 19)
-
-        # work related stuff
-        s101 () {
-          shipper deploy --s101 $1
-        }
-
-        shipthis () {
-          branch=$(eval "git rev-parse --symbolic-full-name --abbrev-ref HEAD")
-          shipper deploy --s101 $branch
-        }
-
-        prod () {
-          shipper deploy --prod $1
-        }
-
-        function mergeship() {
-            local PRNumber=$(gh pr view $(git branch --show-current) --json url --template "{{.url}}") &&\
-            gh pr merge -sd &&\
-            echo "Shipping $PRNumber to production with automated rollback" &&\
-            shipper deploy --s101 --disable-progressive-rollouts --skip-confirm-rollout $PRNumber &&\
-            shipper deploy --prod --skip-confirm-rollout $PRNumber
-        }
-
-        function tpr() {
-            # Check if sufficient arguments are provided
-            if [ $# -lt 1 ]; then
-                echo "Usage: tpr3 [ticket URL or ID] [optional: base branch] [optional: PR title]"
-                return 1
-            fi
-
-            local ticket_id=$(echo "$1" | awk -F '/' '{print $NF}')
-
-            # Setting base branch
-            local base_branch="master"  # Default value
-            if [ -n "$2" ]; then
-                base_branch="$2"
-            fi
-
-            local commit_msg="init"
-            local pr_title="$3"
-            local use_jira_title="yes"
-
-            # Change to "no" if a PR title is provided
-            [ -n "$pr_title" ] && use_jira_title="no"
-
-            # Check if the branch already exists
-            if git rev-parse --verify "$ticket_id" >/dev/null 2>&1; then
-                echo "Branch $ticket_id already exists."
-                return 1
-            fi
-
-            # Fetch ticket details from Jira only if needed
-            if [ "$use_jira_title" = "yes" ]; then
-                # Use environment variables for credentials
-                local jira_domain="https://mondough.atlassian.net"
-                local jira_user="$JIRA_USER"
-                # get your jira api token from here https://id.atlassian.com/manage-profile/security/api-tokens
-                local jira_api_token="$JIRA_API_TOKEN"
-
-                local pr_title=$(curl -s -u "$jira_user:$jira_api_token" \
-                                -H "Content-Type: application/json" \
-                                -X GET \
-                                "$jira_domain/rest/api/latest/issue/$ticket_id" | jq -r '.fields.summary')
-
-                if [ -z "$pr_title" ]; then
-                    echo "Failed to fetch Jira ticket title."
-                    return 1
-                fi
-            fi
-
-            # Checkout the base branch and pull the latest changes
-            git checkout "$base_branch" && git pull || { echo "Failed to checkout and update $base_branch."; return 1; }
-
-            # Create new branch
-            git checkout -b "$ticket_id" || { echo "Failed to create branch $ticket_id."; return 1; }
-
-            # Commit and push
-            git commit -m "$commit_msg" --allow-empty && git push || { echo "Failed to commit and push changes."; return 1; }
-
-            # Create a pull request
-            gh pr create --title "[$ticket_id] $pr_title" --draft --fill || { echo "Failed to create pull request."; return 1; }
-
-            }
       '';
     };
 
