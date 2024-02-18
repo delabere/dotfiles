206e1cda254a231c60d19c556f579e6ca3858790
delabere <jack.rickards@hotmail.co.uk>
2024-02-18 21:06:22 +0000

    add imports to lakeview

--- lakeview.nix
+++ lakeview.nix
@@ -4,6 +4,10 @@
   system,
   ...
 }: {
+  imports = [
+    ./base.nix
+    ./base-apps.nix
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ubuntu";
