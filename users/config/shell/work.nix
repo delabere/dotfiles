2d40cfa39a24def8e0f5c223fc41929f1e6c39f5
Jack Rickards <jackrickards@monzo.com>
2024-02-20 17:18:00 +0000

    update mergeship to use function and call it

--- users/config/shell/work.nix
+++ users/config/shell/work.nix
@@ -19,11 +19,14 @@
   '';
 
   mergeship = pkgs.writeShellScriptBin "mergeship" ''
+  function mergeship() {
     local PRNumber=$(gh pr view $(git branch --show-current) --json url --template "{{.url}}") &&\
     gh pr merge -sd &&\
     echo "Shipping $PRNumber to production with automated rollback" &&\
     shipper deploy --s101 --disable-progressive-rollouts --skip-confirm-rollout $PRNumber &&\
     shipper deploy --prod --skip-confirm-rollout $PRNumber
+  }
+  mergeship
   '';
 
   tpr = pkgs.writeShellScriptBin "tpr" ''
