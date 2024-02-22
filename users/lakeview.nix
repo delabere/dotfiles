b864025fa0df577d8b1de98343dc616b99f98bac
delabere <jack.rickards@hotmail.co.uk>
2024-02-22 21:15:45 +0000

    new base_lite to use with lakeview

--- users/lakeview.nix
+++ users/lakeview.nix
@@ -6,19 +6,10 @@
   ...
 }: {
   imports = [
-    ./config/base.nix
+    ./config/base_lite.nix
     ./config/shell/base.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ubuntu";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
 }
