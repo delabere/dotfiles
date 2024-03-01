803a42f9318ba9e3d43550b6ec582fb5813f0953
Jack Rickards <jackrickards@monzo.com>
2024-03-01 13:43:13 +0000

    add neorg config

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -42,6 +42,7 @@ return {
           }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
+          { name = "neorg" }, -- neorg completion
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "buffer" },
