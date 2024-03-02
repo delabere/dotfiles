dfcc758a5213634a53cf74137ce94b2d9b872458
delabere <jack.rickards@hotmail.co.uk>
2024-03-02 17:01:59 +0000

    add file browser and keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     "axkirillov/telescope-changed-files",
   },
@@ -10,14 +9,31 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      { dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo", "axkirillov/telescope-changed-files" },
+      {
+        dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo",
+        "axkirillov/telescope-changed-files",
+        "nvim-telescope/telescope-file-browser.nvim",
+      },
     },
     config = function()
+      require("telescope").setup({
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+          },
+        },
+      })
+      require("telescope").load_extension("file_browser")
+
       require("telescope").load_extension("changed_files")
     end,
     keys = {
       { "<leader>fs", ":Monzo jump_to_component_no_cd<CR>", desc = "Monzo jump to component" },
       { "<leader>fd", ":Monzo jump_to_downstream<CR>", desc = "Monzo jump to downstream" },
+      { "<leader>E", ":Telescope file_browser<CR>", desc = "File browser at CWD" },
+      { "<leader>e", ":Telescope file_browser path=%:p:h<CR>", desc = "File browser at buffer path" },
     },
   },
 }
