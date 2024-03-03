6185a479f507dca220d38e67bea67c099bfa7a47
delabere <jack.rickards@hotmail.co.uk>
2024-03-03 19:09:17 +0000

    add some obsidian mappings

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,22 +1,13 @@
 return {
   "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  version = "*",
+  lazy = false,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
   dependencies = {
-    -- Required.
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope.nvim" },
     { "hrsh7th/nvim-cmp" },
     { "nvim-treesitter/nvim-treesitter" },
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
@@ -24,10 +15,67 @@ return {
         name = "personal",
         path = "~/vaults/personal",
       },
-      -- {
-      --   name = "work",
-      --   path = "~/vaults/work",
-      -- },
     },
+
+    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+    -- way then set 'mappings = {}'.
+    mappings = {
+      ["gd"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["gr"] = {
+        action = function()
+          return "<cmd>ObsidianBacklinks<CR>"
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["<leader>nn"] = {
+        action = function()
+          return "<cmd>ObsidianNew<CR>"
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["<leader>os"] = {
+        action = function()
+          return "<cmd>ObsidianSearch<CR>"
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["<leader>op"] = {
+        action = function()
+          return "<cmd>ObsidianPasteImg<CR>"
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+    },
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
   },
 }
