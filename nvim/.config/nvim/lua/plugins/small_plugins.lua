02bc5046821d52ee58c8be45ebd4ea96fedf6508
Jack Rickards <jackrickards@monzo.com>
2024-03-04 16:26:58 +0000

    add go if error keymap to vim-plug

--- nvim/.config/nvim/lua/plugins/small_plugins.lua
+++ nvim/.config/nvim/lua/plugins/small_plugins.lua
@@ -64,6 +64,7 @@ return {
     config = function()
       vim.g.go_gopls_enabled = false -- or false to disable
       vim.g.go_def_mapping_enabled = false -- stops vim-go taking over <C-t> for tagstack jumps
+      vim.keymap.set("n", "<leader>ge", "<cmd>GoIfErr<CR>", { desc = "Go if error" })
     end,
   },
 
