5024ea658003d3e74bce1e4d9744bb142bcb061b
Jack Rickards <jackrickards@monzo.com>
2024-03-04 16:34:49 +0000

    add coverage and go to test file

--- nvim/.config/nvim/lua/plugins/small_plugins.lua
+++ nvim/.config/nvim/lua/plugins/small_plugins.lua
@@ -65,6 +65,8 @@ return {
       vim.g.go_gopls_enabled = false -- or false to disable
       vim.g.go_def_mapping_enabled = false -- stops vim-go taking over <C-t> for tagstack jumps
       vim.keymap.set("n", "<leader>ge", "<cmd>GoIfErr<CR>", { desc = "Go if error" })
+      vim.keymap.set("n", "<leader>tf", "<cmd>GoAlternate<CR>", { desc = "Go to tests for file" })
+      vim.keymap.set("n", "<leader>tc", "<cmd>GoCoverageToggle<CR>", { desc = "Test coverage toggle" })
     end,
   },
 
