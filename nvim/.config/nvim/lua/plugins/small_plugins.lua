10b30dd1ccfd856c3c4b8215ae9dd70601988e16
delabere <jack.rickards@hotmail.co.uk>
2024-03-07 21:42:47 +0000

    clean up conform

--- nvim/.config/nvim/lua/plugins/small_plugins.lua
+++ nvim/.config/nvim/lua/plugins/small_plugins.lua
@@ -15,45 +15,17 @@ return {
   },
   {
     "stevearc/conform.nvim",
-    opts = function()
-      ---@class ConformOpts
-      local opts = {
-        -- LazyVim will use these options when formatting with the conform.nvim formatter
-        format = {
-          timeout_ms = 3000,
-          async = false, -- not recommended to change
-          quiet = false, -- not recommended to change
-        },
-        ---@type table<string, conform.FormatterUnit[]>
-        formatters_by_ft = {
-          lua = { "stylua" },
-          fish = { "fish_indent" },
-          sh = { "shfmt" },
-          python = { "ruff_format", "isort" },
-          -- python = { "isort", "black" },
-          go = { "gopls" },
-          nix = { "alejandra" },
-        },
-        -- The options you set here will be merged with the builtin formatters.
-        -- You can also define any custom formatters here.
-        ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-        formatters = {
-          injected = { options = { ignore_errors = true } },
-          -- # Example of using dprint only when a dprint.json file is present
-          -- dprint = {
-          --   condition = function(ctx)
-          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-          --   end,
-          -- },
-          --
-          -- # Example of using shfmt with extra args
-          -- shfmt = {
-          --   prepend_args = { "-i", "2", "-ci" },
-          -- },
-        },
-      }
-      return opts
-    end,
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        python = { "ruff_format", "isort" },
+        -- python = { "isort", "black" },
+        go = { "gopls" },
+        nix = { "alejandra" },
+      },
+    },
   },
 
   { "folke/zen-mode.nvim" },
@@ -88,28 +60,5 @@ return {
     "nvim-neotest/neotest",
     dependencies = { "nvim-neotest/neotest-go" },
     opts = { adapters = { "neotest-go" }, discovery = { enabled = false } },
-  },
-
-  {
-    "stevearc/oil.nvim",
-    keys = {
-      { "<leader>o", "<CMD>Oil<CR>", desc = "Oil" },
-    },
-    opts = {
-      view_options = {
-        -- Show files and directories that start with "."
-        show_hidden = true,
-      },
-
-      -- See :help oil-actions for a list of all available actions
-      keymaps = {
-        ["?"] = "actions.show_help",
-        ["<C-v>"] = "actions.select_vsplit",
-        ["<C-x>"] = "actions.select_split",
-        ["<BS>"] = "actions.parent",
-      },
-    },
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
 }
