4dbff351b5d00292171561d071f1e2a24b22df22
delabere <jack.rickards@hotmail.co.uk>
2024-03-13 20:19:01 +0000

    autocommands to stop autosaving and autoformatting on library files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,14 +2,29 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- autosave
--- vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
---   command = "silent! wa",
--- })
+local function is_readonly()
+  local current_buf = vim.api.nvim_get_current_buf()
+  return vim.api.nvim_buf_get_option(current_buf, "readonly")
+end
+
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "InsertLeave" }, {
+  desc = "autosave modifiable buffers only",
+  callback = function()
+    if is_readonly() then
+      return
+    end
+
+    vim.cmd("silent! wa")
+  end,
+})
 
 vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
-  pattern = "*",
+  desc = "conform autoformat",
   callback = function(args)
+    if is_readonly() then
+      return
+    end
+
     require("conform").format({ bufnr = args.buf })
   end,
 })
