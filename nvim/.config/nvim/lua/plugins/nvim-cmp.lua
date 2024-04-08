6a4d31fc68ed6d0a87d29a329179c5d478818712
Jack Rickards <jackrickards@monzo.com>
2024-04-08 16:53:32 +0100

    add copilot to cmp sources

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -41,6 +41,7 @@ return {
           }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
+          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "buffer" },
