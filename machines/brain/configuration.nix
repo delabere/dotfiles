202fcd8f938abea3cdd4245dda3e6de5cd7ca002
delabere <jack.rickards@hotmail.co.uk>
2024-12-09 23:01:57 +0000

    prune out keys

--- machines/brain/configuration.nix
+++ machines/brain/configuration.nix
@@ -11,6 +11,7 @@
       ./../../services/tailscale.nix
     ];
 
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -91,12 +92,6 @@
   # List services that you want to enable:
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
-  # users.users."delabere".openssh.authorizedKeys.keys = [
-  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzFCNGbA9pwrs6fSxV1A4EG6Kfuzw81GSXoPrVSZSwg delabere@Jacks-MacBook-Pro.local"
-  # ];
-  # users.users.root.openssh.authorizedKeys.keys = [
-  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzFCNGbA9pwrs6fSxV1A4EG6Kfuzw81GSXoPrVSZSwg delabere@Jacks-MacBook-Pro.local"
-  # ];
 
   users.users."delabere".openssh.authorizedKeys.keys =
     let
@@ -109,38 +104,19 @@
       authorizedKeys);
 
   #Open ports in the firewall. networking.firewall.allowedTCPPorts = [ ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the firewall altogether. 
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system (/run/current-system/configuration.nix). This is useful in case you accidentally delete 
-  # configuration.nix. system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine, and is used to maintain compatibility with application data (e.g. databases) 
-  # created on older NixOS versions.
-  #
   # Most users should NEVER change this value after the initial install, for any reason, even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from, so changing it will NOT upgrade your system - see 
-  # https://nixos.org/manual/nixos/stable/#sec-upgrading for how to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration, and migrated your data accordingly.
-  #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 
   programs.vim.enable = true;
 
-  # programs.git = {
-  #   enable = true;
-  #   userName = "delabere";
-  #   userEmail = "jack.rickards@hotmail.co.uk";
-  # };
   programs.zsh.enable = true;
   users.users.delabere.shell = pkgs.zsh;
 
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. wget
+    vim
     stow
     gcc
   ];
