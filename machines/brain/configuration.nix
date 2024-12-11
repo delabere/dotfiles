bc2ea74837ab475e451fc932c876bf86e416677f
delabere <jack.rickards@hotmail.co.uk>
2024-12-11 13:29:04 +0000

    stop gnome suspending

--- machines/brain/configuration.nix
+++ machines/brain/configuration.nix
@@ -35,6 +35,13 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "gb";
