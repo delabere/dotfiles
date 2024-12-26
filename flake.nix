83e0eed0a7d965a071fb090d49a78547261308c6
delabere <jack.rickards@hotmail.co.uk>
2024-12-26 18:32:18 +0000

    use nixpkgs-unstabe
    it has the latest homeassistant which works with my devices

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of delabere";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
